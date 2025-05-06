c605fbdcaed81bb8902a25cd22a5a1aa7c92f47f
Aude Martin <martin.aude@outlook.fr>
2025-05-06 16:03:11 +0200

    Fix point cors

--- middleware/corsMiddleware.js
+++ middleware/corsMiddleware.js
@@ -1,13 +1,18 @@
-import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 const corsMiddleware = cors({
-  origin: process.env.CORS_ORIGIN, 
-  credentials: true, 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://hydre-frontend.vercel.app',
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 });
 
-export default corsMiddleware;
