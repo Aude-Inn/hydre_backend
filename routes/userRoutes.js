27d1af99351070b26364786e0b3adb15a8d706f9
Aude Martin <martin.aude@outlook.fr>
2025-05-09 13:16:53 +0200

    api road

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import {
-  deleteUser,
   getAllUsers,
   getUser,
   updateUser,
+  deleteUser
 } from "../controllers/userController.js";
 
 const router = express.Router();
@@ -12,11 +12,11 @@ const router = express.Router();
 // Récupérer le profil de l'utilisateur connecté
 router.get("/me", protect, getUser);
 
-// Récupérer tous les utilisateurs
-router.get("/", protect, getAllUsers);
-
 // Mettre à jour le profil de l'utilisateur connecté
 router.put("/me", protect, updateUser);
+
+// Récupérer tous les utilisateurs
+router.get("/", protect, getAllUsers);
 
 // Supprimer un utilisateur par son ID (admin)
 router.delete("/:id", protect, deleteUser);
