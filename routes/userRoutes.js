ee09813e100f5199a449957395efae075843c49d
Aude Martin <martin.aude@outlook.fr>
2025-05-15 09:20:14 +0200

    modif update user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,6 @@ import { protect } from "../middleware/authMiddleware.js";
 import {
   getAllUsers,
   getUser,
-  updateUser,
   updateUserById,
   deleteUser
 } from "../controllers/userController.js";
@@ -12,9 +11,6 @@ const router = express.Router();
 
 // Profil user co
 router.get("/me", protect, getUser);
-
-// // Update user(a verifier si util)
-// router.put("/me", protect, updateUser);
 
 // all user
 router.get("/", protect, getAllUsers);
