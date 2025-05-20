4416b04cb9f26512f1b054762059d59f3cba06bb
Aude Martin <martin.aude@outlook.fr>
2025-05-20 13:38:26 +0200

    notif road

--- routes/notifRoutes.js
+++ routes/notifRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getNotifications } from "../controllers/notificationController.js";
+import { getNotifications } from "../controllers/notifController.js";
 
 const router = express.Router();
 
