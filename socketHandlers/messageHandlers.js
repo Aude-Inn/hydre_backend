f15467ab799edf7c8433b9b2f321a5aeeac57e0b
Aude Martin <martin.aude@outlook.fr>
2025-05-22 21:48:53 +0200

    messagehandler filtre userid

--- socketHandlers/messageHandlers.js
+++ socketHandlers/messageHandlers.js
@@ -4,21 +4,23 @@ import User from "../models/User.js";
 const messageHandlers = (io, socket) => {
   console.log("[Server] Nouvelle connexion socket:", socket.id);
 
-  socket.on("request_history", async () => {
+    socket.on("request_history", async () => {
     console.log("[Server] Historique demandé");
     try {
       const messages = await Message.find().sort({ timestamp: 1 });
       const userIds = [...new Set(messages.map((msg) => msg.userId))];
-      const users = await User.find({ _id: { $in: userIds } });
+      const validUserIds = userIds.filter(id => mongoose.Types.ObjectId.isValid(id));
+      const users = await User.find({ _id: { $in: validUserIds } });
 
       const userMap = new Map(users.map((user) => [user._id.toString(), user.name]));
 
       const enrichedMessages = messages.map((msg) => ({
         ...msg.toObject(),
-        userName: userMap.get(msg.userId) || "Utilisateur inconnu",
+        userName: userMap.get(msg.userId) || (msg.userId === "admin" ? "Admin" : "Utilisateur inconnu"),
       }));
 
-      console.log("[Server] Envoi de l'historique :", enrichedMessages);
+      console.log("[Server] Envoi de l'historique :", enrichedMessages.length, "messages");
+
       socket.emit("message_history", enrichedMessages);
     } catch (error) {
       console.error("[Server] Erreur historique :", error);
